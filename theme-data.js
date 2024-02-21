e0e245c6e50bf22ccf4f95675ccfe976aa4a9280
hseager <harvey.seager@googlemail.com>
2024-02-21 19:04:52 +0000

    Added theme data

---
+++ theme-data.js
@@ -0,0 +1,75 @@
+// Please fill in the below object with the lore from
+// Merlin
+const theme = {
+    mainCharacter: "Merlin",
+    characters: [
+        "Morgana",
+        "Mordred",
+        "Arthur Pendragon",
+        "Gaius",
+        "Guinevere (Gwen)",
+        "Uther Pendragon"
+    ], // Excluding main character
+    locations: [
+        {
+            name: "Camelot Castle",
+            description: "Majestic fortress atop rugged cliffs, echoing tales of chivalry."
+        },        
+        {
+            name: "Camelot Tavern",
+            description: "Lively hub in Camelot's heart, offering hearty meals and merry company."
+        },        
+        {
+            name: "Forest of Balor",
+            description: "Verdant labyrinth cloaked in ancient mystery, hiding forgotten secrets."
+        },        
+        {
+            name: "Darkling Woods",
+            description: "Eerie woods of looming darkness, where shadows dance and secrets whisper."
+        }
+    ],
+    items: [
+        "Morgana's Amulet",
+        "Arthur's Polished Armour",
+        "Excalibur's Scabbard",
+        "Gaius's Potion Vial",
+        "Merlin's Dragonlord Staff",
+        "Nimueh's Dark Tome",
+        "Morgause's Enchanted Dagger",
+        "The Cup of Life",
+        "The Crystal of Neahtid",
+        "The Horn of Cathbhadh"
+    ],
+    enemies: [
+        {
+            name: "Morgana's Shadow Assassin",
+            life: "18", // Range 16 - 24
+            attack: "6" // Range 4 - 7
+        },
+        {
+            name: "Uther's Undead Knight",
+            life: "20",
+            attack: "5"
+        },
+        {
+            name: "Nimueh's Water Elemental",
+            life: "17",
+            attack: "7"
+        },
+        {
+            name: "The Great Dragon Kilgharrah",
+            life: "22",
+            attack: "6"
+        },
+        {
+            name: "The Dorocha",
+            life: "19",
+            attack: "5"
+        },
+        {
+            name: "The Knights of Medhir",
+            life: "21",
+            attack: "7"
+        }
+    ]
+}
