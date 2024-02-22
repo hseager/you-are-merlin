ab7498acb7728c6a73a77b2426d6ff52f1d2c577
hseager <harvey.seager@googlemail.com>
2024-02-22 10:47:46 +0000

    Added theme structure

--- src/theme.rs
+++ src/theme.rs
@@ -1,4 +1,18 @@
 pub struct Theme {
     main_character: String,
-    characters: [String; 5]
+    characters: [String; 5],
+    locations: [Location; 4],
+    items: [String; 10],
+    enemies: [Enemy; 6]
+}
+
+struct Location {
+    name: String,
+    description: String
+}
+
+struct Enemy {
+    name: String,
+    life: i16,
+    attack: i8
 }
