291f7ed2e7d4ae62deeeb3cef9529a940f24f81a
hseager <harvey.seager@googlemail.com>
2024-03-13 17:29:25 +0000

    Add main quest

--- src/game_manager/world_builder.rs
+++ src/game_manager/world_builder.rs
@@ -2,7 +2,7 @@ use rand::{seq::SliceRandom, thread_rng};
 
 use crate::{
     config::SIDE_QUEST_COUNT,
-    encounter::{Battle, Encounter, Quest, SideQuest},
+    encounter::{Battle, Encounter, MainQuest, Quest, SideQuest},
     enemy::Enemy,
     location::Location,
     theme::{Theme, ThemeLocation},
@@ -12,8 +12,9 @@ use crate::{
 // TODO add boss encounter
 
 pub fn build_world(theme: &Theme) -> Vec<Location> {
-    let locations = build_locations(&theme);
+    let mut locations = build_locations(&theme);
 
+    create_main_quest(&theme, &mut locations);
     generate_side_quests(&theme, locations, SIDE_QUEST_COUNT)
 }
 
@@ -36,9 +37,9 @@ fn build_locations(theme: &Theme) -> Vec<Location> {
     locations
 }
 
+// Fill each location with 3 battle encounters
 fn build_encounters(theme_location: &ThemeLocation) -> Vec<Encounter> {
-    // Fill encounters with battles by default
-    let encounters = theme_location
+    theme_location
         .enemies
         .map(|enemy| {
             let battle: Battle = Battle {
@@ -51,17 +52,18 @@ fn build_encounters(theme_location: &ThemeLocation) -> Vec<Encounter> {
 
             Encounter::Battle(battle)
         })
-        .to_vec();
-
-    encounters
+        .to_vec()
 }
 
+// Add side quests to random locations
 fn generate_side_quests(
     theme: &Theme,
     mut locations: Vec<Location>,
     quest_count: usize,
 ) -> Vec<Location> {
     let mut rng = thread_rng();
+
+    locations.shuffle(&mut rng);
 
     for location in locations.iter_mut().take(quest_count) {
         let quest = build_side_quest(theme);
@@ -73,6 +75,25 @@ fn generate_side_quests(
     locations
 }
 
+// Add a the main quest to a random location
+fn create_main_quest(theme: &Theme, locations: &mut Vec<Location>) -> () {
+
+    // TODO exclude character from sidequests 
+    let quest = Quest::MainQuest(MainQuest {
+        character: theme
+            .characters
+            .get(get_random_array_index(&theme.characters))
+            .unwrap(),
+    });
+
+    let mut rng = thread_rng();
+
+    locations.shuffle(&mut rng); // TODO group shuffling
+
+    if let Some(location) = locations.get_mut(0) {
+        location.encounters.insert(0, Encounter::Quest(quest));
+    }
+}
 
 fn build_side_quest(theme: &Theme) -> Encounter {
     let side_quest = SideQuest {
