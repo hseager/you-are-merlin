429649deb05579bd104115997e2cebf8daaccbf5
hseager <harvey.seager@googlemail.com>
2024-04-12 19:54:06 +0100

    Small refactor to item gen

--- src/config.rs
+++ src/config.rs
@@ -11,3 +11,7 @@ pub const ENEMY_EASY_STATS: (i16, u16) = (20, 3);
 pub const ENEMY_MEDIUM_STATS: (i16, u16) = (26, 5);
 pub const ENEMY_HARD_STATS: (i16, u16) = (38, 6);
 pub const ENEMY_BOSS_STATS: (i16, u16) = (150, 10);
+
+// min, max
+pub const ITEM_GEN_ATTACK: (u16, u16) = (1, 4);
+pub const ITEM_GEN_LIFE: (i16, i16) = (5, 10);
