54b013132ca465d8332ab1d2b9d8a4e40297f110
hseager <harvey.seager@googlemail.com>
2024-04-24 20:22:03 +0100

    Slowed battles down

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,6 @@
 pub const PLAYER_LIFE: i16 = 100;
 pub const PLAYER_ATTACK: u16 = 10;
-pub const PLAYER_ATTACK_SPEED: u16 = 2000; // DPS: 5
+pub const PLAYER_ATTACK_SPEED: u16 = 4000;
 
 pub const REST_HEAL_AMOUNT: i16 = 10;
 pub const REST_INTERVAL_MILLIS: u32 = 2000;
@@ -8,10 +8,10 @@ pub const REST_INTERVAL_MILLIS: u32 = 2000;
 pub const BATTLE_INTERVAL_MILLIS: u32 = 100;
 
 // life, attack, attack speed (millisecs)
-pub const ENEMY_EASY_STATS: (i16, u16, u16) = (20, 3, 1000); // DPS: 3
-pub const ENEMY_MEDIUM_STATS: (i16, u16, u16) = (26, 5, 1200); // DPS: 6
-pub const ENEMY_HARD_STATS: (i16, u16, u16) = (38, 14, 1800); // DPS: 7.7
-pub const ENEMY_BOSS_STATS: (i16, u16, u16) = (100, 8, 1000); // DPS: 8
+pub const ENEMY_EASY_STATS: (i16, u16, u16) = (20, 7, 3800);
+pub const ENEMY_MEDIUM_STATS: (i16, u16, u16) = (25, 9, 3500);
+pub const ENEMY_HARD_STATS: (i16, u16, u16) = (28, 12, 3200);
+pub const ENEMY_BOSS_STATS: (i16, u16, u16) = (100, 20, 3000);
 
 // min, max
 pub const ITEM_GEN_ATTACK: (u16, u16) = (1, 4);
